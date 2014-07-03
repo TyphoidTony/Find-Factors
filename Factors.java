b430440e75c6d497cb4306fef31e901ce1a394b3
TyphoidTony <stuthagen@gmail.com>
2014-07-03 12:45:02 -0400

    The program
    Prints the factors of a given number and catches simple errors

---
+++ Factors.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+/**
+ *Created on 7/3/2014.
+ *
+ * Wanted to quickly make a command-line tool that will print the factors of a given number.
+ * Simple, nothing ground-breaking here, all just for fun.
+ *
+ */
+
+public class Factors {
+
+
+
+    public Factors() {
+
+        String userInput;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a number you would like to find the factors for \n" +
+                "If you would like to quit type: quit");
+        do {
+
+            userInput = in.next();
+            factorsOf(userInput);
+            System.out.print("\n\n");
+
+        }while(in.hasNext());
+
+
+    }
+
+    public void factorsOf(String s){
+        if(s.equalsIgnoreCase("quit")){
+            System.exit(0);
+        }
+            int n;
+        try {
+            n = Integer.parseInt(s);
+        }catch(NumberFormatException nfe){
+            System.out.println("Somewhere along the line you didn't enter a number try again");
+            return;
+        }
+        System.out.print("\nThe factors are: ");
+        for(int i = 1; i<=n; i++){
+
+            if(n%i==0){
+                System.out.print(i+", ");
+            }
+
+        }
+
+    }
+
+
+    public static void main(String[] args){
+        new Factors();
+    }
+}
